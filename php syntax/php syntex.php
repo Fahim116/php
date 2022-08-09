20507c5a18e871f9860ff6a6f90a8aba5148231d
Fahim116 <102754991+Fahim116@users.noreply.github.com>
2022-08-09 13:14:45 +0600

    php syntax

---
+++ php syntax/php syntex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <?php
+    $color= red;
+    echo "my car is " . $color . <"br">;
+    echo "my phone is  " .$color . <"br">;
+    echo "This pane is  " . % color . <"br">;
+    ?>
+</body>
+</html>
